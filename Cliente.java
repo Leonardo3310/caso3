3b82c045101650fd18d1a90c69e575d8c80273fb
“Leonardo”3310 <jl.rangel@uniandes.edu.co>
2024-05-02 09:02:24 -0500

    instrucciones para cliente

--- Cliente.java
+++ Cliente.java
@@ -6,13 +6,26 @@ public class Cliente {
         String hostName = "localhost"; // Dirección del servidor
         int port = 1234; // Puerto del servidor
 
-        try (Socket socket = new Socket(hostName, port);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+        try{
+            Socket socket = new Socket(hostName, port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            out.println("42"); // Enviar un número al servidor
+            String numero = "3";
+            out.println(numero); // Enviar un número al servidor
             String response = in.readLine(); // Leer la respuesta del servidor
             System.out.println("Respuesta del servidor: " + response);
+
+            // Hacer un ciclo en el cual cuando recibe el mensaje entonce verifique que si es el que espere y conteste con lo que sea que tiene que contestar
+            // Luego enviar el mensaje que se espera y verificar que la respuesta sea la essperada
+            // esperar ahora los datos que no tiene que enviar el servidor, como el p y q y esas madres 
+            // verificar el cifrado que nos manda el servidor 
+            // contestar si ese es el cifrado que esperaabamos 
+            // enviar clave G A LA Y, revisa la cosa de el algoritmo de hiffie eldman alguna madre asi 
+
+
+
+
         } catch (UnknownHostException e) {
             System.err.println("No se conoce sobre el host " + hostName);
             System.exit(1);
